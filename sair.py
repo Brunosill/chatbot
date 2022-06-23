a8dfe5e326378b415ca10fdf427d2c749e902476
Guilherme Fontoura <guilhermefontoura68@gmail.com>
2022-06-23 13:30:53 -0300

    função de feedback/saída revisada

--- sair.py
+++ sair.py
@@ -1,10 +1,10 @@
 def sair():
     text = input('Sair (s/n)?')
     if(text.lower() == 's'):
-        feedb = input("Deseja deixar um feedback (s/n)? ")
+        feedb = input("Deseja deixar um feedback (s/n)?")
         if(feedb.lower() == 's'):
             salvarFeedback()
-            print("Obrigado pelo seu FeedBack e ate mais! :)")
+            print("Obrigado pelo seu FeedBack e até mais! :)")
             return False
         else:
             print("Até mais! :)")
